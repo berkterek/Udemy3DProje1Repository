78a5be0e90aa3ca3a26b79ed54245862c15c2f84
berkterek <42406263+berkterek@users.noreply.github.com>
2021-01-19 13:52:55 +0300

    PlayerController
    PlayerContoller eklendi ve player prefab'ine boxcollider rigidbody eklendi ve new input controller olusturuldu

---
+++ UdemyProject1/Assets/GameFolders/Scripts/Concretes/Inputs/DefaultAction.cs
@@ -0,0 +1,135 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/GameFolders/Scripts/Concretes/Inputs/DefaultAction.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+namespace UdemyProject1.Inputs
+{
+    public class @DefaultAction : IInputActionCollection, IDisposable
+    {
+        public InputActionAsset asset { get; }
+        public @DefaultAction()
+        {
+            asset = InputActionAsset.FromJson(@"{
+    ""name"": ""DefaultAction"",
+    ""maps"": [
+        {
+            ""name"": ""Rocket"",
+            ""id"": ""6ce9b8be-5f75-4214-987d-6cb7c7cc666c"",
+            ""actions"": [
+                {
+                    ""name"": ""ForceUp"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""fb7fb18c-f1cd-4d0d-9319-fe98dece4bdd"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""564b4121-0f43-4118-9da2-69251ebd6ac6"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""ForceUp"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+            // Rocket
+            m_Rocket = asset.FindActionMap("Rocket", throwIfNotFound: true);
+            m_Rocket_ForceUp = m_Rocket.FindAction("ForceUp", throwIfNotFound: true);
+        }
+
+        public void Dispose()
+        {
+            UnityEngine.Object.Destroy(asset);
+        }
+
+        public InputBinding? bindingMask
+        {
+            get => asset.bindingMask;
+            set => asset.bindingMask = value;
+        }
+
+        public ReadOnlyArray<InputDevice>? devices
+        {
+            get => asset.devices;
+            set => asset.devices = value;
+        }
+
+        public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+        public bool Contains(InputAction action)
+        {
+            return asset.Contains(action);
+        }
+
+        public IEnumerator<InputAction> GetEnumerator()
+        {
+            return asset.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Enable()
+        {
+            asset.Enable();
+        }
+
+        public void Disable()
+        {
+            asset.Disable();
+        }
+
+        // Rocket
+        private readonly InputActionMap m_Rocket;
+        private IRocketActions m_RocketActionsCallbackInterface;
+        private readonly InputAction m_Rocket_ForceUp;
+        public struct RocketActions
+        {
+            private @DefaultAction m_Wrapper;
+            public RocketActions(@DefaultAction wrapper) { m_Wrapper = wrapper; }
+            public InputAction @ForceUp => m_Wrapper.m_Rocket_ForceUp;
+            public InputActionMap Get() { return m_Wrapper.m_Rocket; }
+            public void Enable() { Get().Enable(); }
+            public void Disable() { Get().Disable(); }
+            public bool enabled => Get().enabled;
+            public static implicit operator InputActionMap(RocketActions set) { return set.Get(); }
+            public void SetCallbacks(IRocketActions instance)
+            {
+                if (m_Wrapper.m_RocketActionsCallbackInterface != null)
+                {
+                    @ForceUp.started -= m_Wrapper.m_RocketActionsCallbackInterface.OnForceUp;
+                    @ForceUp.performed -= m_Wrapper.m_RocketActionsCallbackInterface.OnForceUp;
+                    @ForceUp.canceled -= m_Wrapper.m_RocketActionsCallbackInterface.OnForceUp;
+                }
+                m_Wrapper.m_RocketActionsCallbackInterface = instance;
+                if (instance != null)
+                {
+                    @ForceUp.started += instance.OnForceUp;
+                    @ForceUp.performed += instance.OnForceUp;
+                    @ForceUp.canceled += instance.OnForceUp;
+                }
+            }
+        }
+        public RocketActions @Rocket => new RocketActions(this);
+        public interface IRocketActions
+        {
+            void OnForceUp(InputAction.CallbackContext context);
+        }
+    }
+}
